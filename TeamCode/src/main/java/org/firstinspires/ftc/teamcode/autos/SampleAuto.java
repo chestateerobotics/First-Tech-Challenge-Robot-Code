27a52299580686558121d4d291db31ddc8fb01db
BralinGar <bralingarcia7@outlook.com>
2022-11-14 13:44:55 -0500

    Updated Auto
    trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/SampleAuto.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.autos;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.classes.WebcamClass;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
 import java.util.List;
 import java.util.Objects;
@@ -12,10 +16,19 @@ import java.util.Objects;
 @Autonomous
 public class SampleAuto extends LinearOpMode
 {
+    String objDetect = "";
     @Override
     public void runOpMode()
     {
         WebcamClass camera = new WebcamClass(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(new Pose2d(0, 0, Math.toRadians(90)));
+
+        Trajectory startMove = drive.trajectoryBuilder(new Pose2d(0,0, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(0, 30), Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(-25, 30, Math.toRadians(104)), Math.toRadians(180))
+                .lineToLinearHeading(new Pose2d(10, -55, Math.toRadians(104)))
+                .build();
 
         while(!opModeIsActive() && camera.getTfod() != null)
         {
@@ -24,11 +37,11 @@ public class SampleAuto extends LinearOpMode
                 telemetry.addData("# Objects Detected", updatedRecognitions.size());
                 for (Recognition recognition : updatedRecognitions) {
                     if(Objects.equals(recognition.getLabel(), "cargill1"))
-                        telemetry.addData("cargill for life", "");
+                        objDetect = "cargill1";
                     else if(Objects.equals(recognition.getLabel(), "number2"))
-                        telemetry.addData("number for life", "");
+                        objDetect = "number2";
                     else
-                        telemetry.addData("eagle for life", "");
+                        objDetect = "eagle3";
                     telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100 );
                 }
                 telemetry.update();
@@ -38,7 +51,7 @@ public class SampleAuto extends LinearOpMode
         waitForStart();
         while(opModeIsActive())
         {
-
+            drive.followTrajectory(startMove);
         }
     }
 }
